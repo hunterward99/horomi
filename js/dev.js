97a72d69e491a287e60f5fa3343d396e2686cec4
Pavel Golovin <pavel.golovin09@yandex.ru>
2023-04-18 11:45:16 +0300

    добавил плавность появления bg при наведении на кнопки в секции main, calc. Сделал блок dev (уведомление о разработке) - разметка + стили. Добавил в footer адрес компании и email

---
+++ js/dev.js
@@ -0,0 +1,41 @@
+let dev = document.querySelector('.dev');
+let devBg = document.querySelector('.dev__bg');
+let closeBtn = document.querySelector('.dev__button-close');
+let devBlock = document.querySelector('.dev__block');
+
+document.addEventListener('DOMContentLoaded', () => {
+    showdev(); 
+});
+
+
+export default function showdev() {
+    // dev.style.cssText = 'visibility: visible; opacity: 1; transition: visibility 1s, opacity 1s';
+    dev.style.visibility = 'visible';
+    dev.style.opacity = 1;
+    devBlock.style.cssText = 'transform: translate(-50%, -50%)';
+    dev.style.transition = 'visibility .7s, opacity .7s';
+    devBlock.style.transition = 'transform 1s';
+
+    // devBlock.style.cssText = 'transform: translateY(100%)';
+
+    devBg.addEventListener('click', () => {
+        closedev();
+    })
+    closeBtn.addEventListener('click',() => {
+        closedev();
+    })
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape'){
+            closedev();
+        }
+    })
+    
+}
+
+function closedev() {
+    dev.style.visibility = 'hidden';
+    dev.style.opacity = 0;
+    devBlock.style.cssText = 'transform: translate(-80%, -50%)';
+    devBlock.style.transition = 'transform 1s';
+    dev.style.transition = 'visibility .3s, opacity .3s';
+}
